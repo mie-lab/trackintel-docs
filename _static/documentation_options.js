d3345a828e3e917063181dd43df0dcbc8f59e928
dominik.bucher <dominik.bucher@gmail.com>
2019-02-18 13:38:37 +0100

    Documentation bump.

---
+++ _static/documentation_options.js
@@ -0,0 +1,10 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '&lt;module &#39;trackintel.__version__&#39; from &#39;C:\\Data\\Programming\\PhD\\mie-lab\\trackintel\\trackintel\\__version__.py&#39;&gt;',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+};
